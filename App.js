8675b1247dc043703af1b34bb2347fc538dbf6c2
Felipe Lopes <felipe.presidencia@gmail.com>
2023-01-09 14:19:39 -0300

    C2 - Validação dos campos

--- App.js
+++ App.js
@@ -1,14 +1,39 @@
 /**
  * 
  */
-import React, { useState } from "react";
-import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from "react-native";
+import React, { useState, useRef } from "react";
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity, Keyboard } from "react-native";
 import { styles } from "./styles";
 import api from "./src/services/api";
 
 export default function App() {
 
   const [cep, setCep] = useState('')
+  const inputRef = useRef(null);
+  const [cepUser, setCepUser] = useState(null);
+
+  async function buscar() {
+    if (cep == '') {  //se o cep for vazio cai aqui
+      alert('Digite um CEP válido')
+      setCep('') //volta a ficar vazio
+      return;  //volta para o inicio
+    }
+
+    try {
+      const resposta = await api.get(`/${cep}/json`);  //get busca se o cep que foi digitado é valido
+      //console.log(resposta.data) //apresentará a resposta encontrada na requisição get
+      setCepUser(resposta.data)  //passa para o setCepUser o resultado encontrado em  resposta.data
+      Keyboard.dismiss();  //faz o teclado fechar
+    } catch (error) {
+      console.log('ERROR: ' + error)
+    }
+  }
+
+  function limpar() {
+    setCep('')
+    inputRef.current.focus();  //ao limpar, fará com que o cursor permaneã no input
+    setCepUser(null); //limpa os campos 
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -19,28 +44,31 @@ export default function App() {
           style={styles.input}
           placeholder="Ex: 73370100"
           value={cep}
-          onChangeText={ (texto) => setCep(texto) } //cria o texto, setCep recebera o valor digitado por meio texto
+          onChangeText={(texto) => setCep(texto)} //cria o texto, setCep recebera o valor digitado por meio texto
           keyboardType="numeric"
+          ref={inputRef}
         />
       </View>
 
       <View style={styles.areaBtn}>
-        <TouchableOpacity style={styles.botao}>
+        <TouchableOpacity style={styles.botao} onPress={buscar}>
           <Text style={styles.botaoText}>Buscar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.botao}>
+        <TouchableOpacity style={styles.botao} onPress={limpar}>
           <Text style={styles.botaoText}>Limpar</Text>
         </TouchableOpacity>
       </View>
 
-      <View style={styles.resultado}>
-        <Text style={styles.itemText}>CEP: 73370100</Text>
-        <Text style={styles.itemText}>Logradouro: 73370100</Text>
-        <Text style={styles.itemText}>Bairro: 73370100</Text>
-        <Text style={styles.itemText}>Cidade: 73370100</Text>
-        <Text style={styles.itemText}>Estado: 73370100</Text>
-      </View>
+      {cepUser &&  //evita o erro, caso o cepUser começe como null
+        <View style={styles.resultado}>
+          <Text style={styles.itemText}>CEP: {cepUser.cep}</Text>
+          <Text style={styles.itemText}>Logradouro: {cepUser.logradouro}</Text>
+          <Text style={styles.itemText}>Bairro: {cepUser.bairro}</Text>
+          <Text style={styles.itemText}>Cidade: {cepUser.localidade}</Text>
+          <Text style={styles.itemText}>Estado: {cepUser.uf}</Text>
+        </View>
+      }
     </SafeAreaView>
   )
 }
